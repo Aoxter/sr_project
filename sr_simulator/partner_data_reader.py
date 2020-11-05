8d88e8524221dd960c0adcae5cfd28a9409b4d92
Piotr Kopycki <pkopycki@onet.pl>
2020-11-05 10:39:52 +0100

    partner_data_reader data list update
    Data list with DF changed into list with tuple of DF and partner_id

--- sr_simulator/partner_data_reader.py
+++ sr_simulator/partner_data_reader.py
@@ -6,8 +6,8 @@ class partner_data_reader:
     def __init__(self, partners_id, path_to_data="../data/"):
         # list with all partners_id
         self.partners_id_list = partners_id
-        # list with all partners df in same order as in partners_id_list
-        self.partners_data_list = []
+        # list with tuples with partners id and df
+        self.partners_one_day_data_list = []
         self.data_directory = path_to_data
         # first dates for partners
         self.current_date = None
@@ -42,12 +42,13 @@ class partner_data_reader:
 
     def next_day(self):
         # clear previous day data
-        self.partners_data_list.clear()
+        self.partners_one_day_data_list.clear()
         for partner_id in self.partners_id_list:
-            self.partners_data_list.append(self.get_next_day_partner_data(partner_id, self.current_date))
+            data_partner_tuple = (partner_id, self.get_next_day_partner_data(partner_id, self.current_date))
+            self.partners_one_day_data_list.append(data_partner_tuple)
         # date update
         self.current_date += datetime.timedelta(days=1)
-        return self.partners_data_list
+        return self.partners_one_day_data_list
 
     def get_next_day_partner_data(self, partner_id, day):
         partner_path = self.data_directory + "data_" + partner_id + ".csv"
