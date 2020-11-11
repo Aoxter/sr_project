3919459ee7fedc802fe8c20ff87b2c10557e233b
Piotr Kopycki <pkopycki@onet.pl>
2020-11-11 15:37:44 +0100

    simulator_core update

--- sr_simulator/simulator_core.py
+++ sr_simulator/simulator_core.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from partner_data_reader import partner_data_reader
+from per_partner_simulator import per_partner_simulator
 
 class simulator_core:
     def __init__(self, partners_to_involve_in_simulation, partners_to_not_involve_in_simulation, path_to_data="../data/"):
+        self.partners_to_involve_in_simulation = partners_to_involve_in_simulation
+        self.partners_to_not_involve_in_simulation = partners_to_not_involve_in_simulation
         # list with all partners_id
         self.partners_to_read_data_from = partners_to_involve_in_simulation + partners_to_not_involve_in_simulation
         # list with all partners df in same order as in partners_id_list
@@ -10,12 +13,23 @@ class simulator_core:
         self.data_directory = path_to_data
         # list of optimizers for each partner
         self.product_list_optimizer = []
-        # TODO call per_partner_simulator init
+        for partner_id in self.partners_to_involve_in_simulation:
+            self.product_list_optimizer.append(per_partner_simulator(partner_id))
         self.pdr = partner_data_reader(self.partners_to_read_data_from, self.data_directory)
 
     def next_day(self):
         self.partners_one_day_data_list = self.pdr.next_day()
+        for plo in self.product_list_optimizer:
+            plo.next_day(self.filter_out_other_partners_data(plo.partner_id))
         # split_many_partners_data - podzieli self.partners_one_day_data_list
         # split_partner_data - podzieli dane pojedynczych partnerów
         # split_df - podzili df w ramach funkcji wyżej
-        # wywołanie next optimizer dla każdego partnera oddzielnie
+        # TODO call next day from per_partner
+        # TODO return gain
+
+    def filter_out_other_partners_data(self, partner_id):
+        for tuple in self.partners_one_day_data_list:
+            if partner_id == tuple[0]:
+                return tuple[1]
+        # TODO return empty DF?
+        return None
