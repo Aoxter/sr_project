a651403df16a432ac877b7b782b6673b118c0506
Piotr Kopycki <pkopycki@onet.pl>
2020-11-11 15:38:12 +0100

    partner_profiles added

---
+++ sr_simulator/partners_profiles.py
@@ -0,0 +1,11 @@
+
+
+class partners_profiles:
+    def __init__(self):
+        None
+
+    def read_partners_profiles(self):
+        None
+
+    def read_partners_profiles_from_file(self):
+        None
