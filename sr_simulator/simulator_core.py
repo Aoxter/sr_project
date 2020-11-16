948036d999e2340ea68e6c204b072731fe932e2e
Piotr Kopycki <piotr.kopycki@itti.com.pl>
2020-11-16 12:40:43 +0100

    Restructurization of project folders

--- sr_simulator/simulator_core.py
+++ sr_simulator/simulator_core.py
@@ -1,20 +1,17 @@
-import pandas as pd
-from partner_data_reader import partner_data_reader
-from per_partner_simulator import per_partner_simulator
+from sr_simulator.partner_data_reader import partner_data_reader
+from sr_simulator.per_partner_simulator import per_partner_simulator
 
 class simulator_core:
-    def __init__(self, partners_to_involve_in_simulation, partners_to_not_involve_in_simulation, path_to_data="../data/"):
+    def __init__(self, partners_to_involve_in_simulation, partners_to_read_data_from, NPM, seed, UCB_beta, path_to_data="../data/"):
         self.partners_to_involve_in_simulation = partners_to_involve_in_simulation
-        self.partners_to_not_involve_in_simulation = partners_to_not_involve_in_simulation
-        # list with all partners_id
-        self.partners_to_read_data_from = partners_to_involve_in_simulation + partners_to_not_involve_in_simulation
+        self.partners_to_read_data_from = partners_to_read_data_from
         # list with all partners df in same order as in partners_id_list
         self.partners_one_day_data_list = []
         self.data_directory = path_to_data
         # list of optimizers for each partner
         self.product_list_optimizer = []
         for partner_id in self.partners_to_involve_in_simulation:
-            self.product_list_optimizer.append(per_partner_simulator(partner_id))
+            self.product_list_optimizer.append(per_partner_simulator(partner_id, NPM, seed, UCB_beta))
         self.pdr = partner_data_reader(self.partners_to_read_data_from, self.data_directory)
 
     def next_day(self):
@@ -24,7 +21,6 @@ class simulator_core:
         # split_many_partners_data - podzieli self.partners_one_day_data_list
         # split_partner_data - podzieli dane pojedynczych partnerów
         # split_df - podzili df w ramach funkcji wyżej
-        # TODO call next day from per_partner
         # TODO return gain
 
     def filter_out_other_partners_data(self, partner_id):
