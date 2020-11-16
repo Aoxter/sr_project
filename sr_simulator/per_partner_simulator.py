7176ea43f08446ee364f4c602cf75501e60e5fba
Piotr Kopycki <piotr.kopycki@itti.com.pl>
2020-11-16 12:42:40 +0100

    per_partner_simulator fix

--- sr_simulator/per_partner_simulator.py
+++ sr_simulator/per_partner_simulator.py
@@ -1,19 +1,33 @@
 import pandas as pd
+from sr_optimizer.optimizer import optimizer
 
 class per_partner_simulator:
-    def __init__(self, partner_id):
+    def __init__(self, partner_id, NPM, seed, UCB_beta):
         self.partner_id = partner_id
         self.partner_data_df = None
-        # TODO create optimizer instance for partner
+        self.click_saving = 0.0
+        self.sale_losses = 0.0
+        self.profil_losses = 0.0
+        self.profil_gain = 0.0
+        self.yesterday_excluded = []
+        self.next_day_excluded = []
+        self.first_day_flag = True
+        self.optimizer = optimizer(NPM, seed, UCB_beta)
 
     def next_day(self, partner_data_df):
         self.partner_data_df = partner_data_df
-        # TODO call optimizer next day and return (excluded, date)
-        optimized_tuple = (None, None)
-        return self.calculate_per_day_profit_gain_factors(optimized_tuple[0])
+        # update yesterday excluded list
+        if self.first_day_flag:
+            self.first_day_flag = False
+        else:
+            self.yesterday_excluded = self.next_day_excluded
+        # get next day excluded
+        optimized_tuple = self.optimizer.next_day(self.partner_data_df)
+        self.next_day_excluded = optimized_tuple[1]
+        return self.calculate_per_day_profit_gain_factors()
 
     def filter_out_other_partners_data(self):
         None
 
-    def calculate_per_day_profit_gain_factors(self, excluded_products):
+    def calculate_per_day_profit_gain_factors(self):
         None
