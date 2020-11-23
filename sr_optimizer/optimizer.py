66a8df59b2d398f5229b85beb86be8ddbc08df2a
Piotr Kopycki <piotr.kopycki@itti.com.pl>
2020-11-23 14:11:29 +0100

    optimizer update

--- sr_optimizer/optimizer.py
+++ sr_optimizer/optimizer.py
@@ -1,11 +1,35 @@
-
+import random
 
 class optimizer:
-    def __init__(self, NPM, seed, UCB_beta):
-        self.next_day_data = None
+    def __init__(self, NPM, seed, how_many_ratio, UCB_beta):
         self.NPM = NPM
         self.seed = seed
+        self.how_many_ratio = how_many_ratio
         self.UCB_beta = UCB_beta
+        self.products = []
+        self.date = None
 
     def next_day(self, next_day_data):
-        None
+        self.products = self.__get_products_seen_today(next_day_data)
+        if next_day_data.empty:
+            excluded_products = []
+        else:
+            #print("Next day:", next_day_data['click_timestamp'][0])
+            #self.date = next_day_data['click_timestamp'][0]
+            excluded_products = self.__get_excluded_products_pseudorandomly()
+        return excluded_products
+
+    def __get_excluded_products_pseudorandomly(self):
+        dummy_list_of_potentially_excluded_products = self.products
+        dummy_how_many_products = round(len(dummy_list_of_potentially_excluded_products) / self.how_many_ratio)
+        random.seed(self.seed)
+        excluded_products = random.sample(dummy_list_of_potentially_excluded_products, dummy_how_many_products)
+        if excluded_products == None:
+            excluded_products = []
+        #return self.date, excluded_products
+        return excluded_products
+
+    def __get_products_seen_today(self, data_df):
+        products = data_df['product_id'].unique().tolist()
+        return products
+
